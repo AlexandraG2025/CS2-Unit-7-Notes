625f39fca39420e601772d90f6eff9e384bc3631
AlexandraG2025 <Alexandra.greenholtz2025@gbwl.org>
2025-02-06 13:53:42 +0000

    watch list

--- Main.java
+++ Main.java
@@ -56,8 +56,74 @@ public class Main {
       System.out.println(lastElement);
       System.out.println(toBuy.get(3));
 
+      // .set(int index, obj) REPLACES element at that index with the new obj
+      // returns the replaced element (same type as the list holds)
+      String replacedElement = toBuy.set(1, "Baked Cheetos");
+      System.out.println(toBuy);
+      // Often, you won't need to store the return output
+      toBuy.set(2, "Iced Tea"); // like calling a VOID method
+      System.out.println(toBuy);
 
+      // .remove(int index) DELETES element at that index
+      // returns the removed object (same type as the list holds)
+      String removedElement = toBuy.remove(0);
+      System.out.println(toBuy);
+      System.out.println("Moved to shopping cart: " + removedElement);
+      // ALSO shifts all remaining elements to the left (lower index)
+      // Often, you won't need to actually store the removed item
+      toBuy.remove(3);
+      System.out.println(toBuy);
+
+      // 7.3 Traversing with Loops
+      ArrayList<Integer> bingoNumbers = new ArrayList<Integer>();
+      bingoNumbers.add(99);
+      bingoNumbers.add(29);
+      bingoNumbers.add(77);
+      bingoNumbers.add(13);
+
+      // ENHANCED FOR-EACH LOOP
+      int total = 0;
+      int product = 1;
+      for (Integer num : bingoNumbers) {
+         total = total + num; // same as total += num
+         product *= num;
+      }
+      System.out.println("Sum of bingo numbers: " + total);
+      System.out.println("Product of bingo numbers: " + product);
       
+      
+      // STANDARD FOR LOOP (indexed)
+      int sum = 0;
+      // DIFFERENT from Arrays;
+      // 1. use ArrayList.size() insetead of Array.length
+      //2. use ArrayList.get(i) instead of Array[i]
+      for (int i=0; i < bingoNumbers.size(); i++) {
+         sum = sum + bingoNumbers.get(i);
+      }
+      System.out.println(sum);
+
+      //WHILE LOOPS (for conditonals, when you don't know exactly how many times to loop)
+      ArrayList<String> watchList = new ArrayList<String>();
+      watchList.add("The OC");
+      watchList.add("Traitors");
+      watchList.add("Grey's Anatomy");
+      watchList.add("Nobody wants this");
+
+      // Example: search and remove
+      boolean found = false; // haven't searched yet
+      int index = 0; // need index to use list methods
+      String watchedShow = "Grey's Anatomy";
+      // loop while current index is still less than list size
+      while (index < watchList.size()) {
+         if (watchedShow.equals(watchList.get(index))) {
+            watchList.remove(index);
+            found = true;
+         }
+         else {
+            index++;
+         }
+      }
+      System.out.println(watchList);
 
 
 
